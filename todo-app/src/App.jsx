a238101bb2da8e43994179c9237a129215a13c6d
aabrahamjay <aabrahamjay@gmail.com>
2025-09-18 13:09:16 +0100

    first function

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+
+export default function App (){
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+  const addTask = (e) => {
+    e.preventDefault();
+
+    if (task.trim() === "") {
+      alert("task is empty");
+    }
+    setTodos ((prev)=> [...prev, 
+      { id:Date.now(), text:task.trim(), done:false},])
+  };
+  
+  return ( <div>
+  <h1>Simple Todo</h1>
+
+  <form onSubmit={addTask}>
+  <input type="text" 
+   value={task} 
+   onChange={(e)=> setTask(e.target.value)} 
+    placeholder="Enter task..." />
+  <button type="submit">Add</button>
+  </form>
+
+  <ul>
+    {todos.length === 0 ? (
+      <li>no task to display</li>
+    ):(
+      todos.map((todo) => <TodoItem  todo={todo} key={todo.id} />   )
+    )}
+  </ul>
+
+</div>
+
+
+)
+}
